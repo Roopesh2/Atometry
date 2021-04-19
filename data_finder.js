949e77c6e6584d688516ed943a5b8461a860a0bc
Roopesh <roopeshor6@gmail.com>
2021-04-19 21:51:32 +0530

    added files

---
+++ data_finder.js
@@ -0,0 +1,16 @@
+/* finds group, period, electronic configuration, subshell electronic configuration,
+   block and category from atomic number
+*/
+
+/**
+ * get subshell electronic configuration from atomic numeber
+ * @returns {string} subshell sequence
+ */
+function getSubElConfig (z) {
+  // max e⁻ in every subshells
+  var max = {s: 2, p: 6, d: 10, f: 14};
+  // order of filling of electrons
+  var order = [
+    
+  ]
+}
